7a12b39d8677cb03aa80c7f872cc7b316659c705
Harry Zuzan <iliketobicycle@gmail.com>
2016-05-20 21:09:01 -0700

    got rid of the mrf code

---
+++ redescendl/__init__.py
@@ -0,0 +1,11 @@
+#
+#from mrf_wavelet_icm import shrink_mrf2_redescend as bws2d_redescend
+#from mrf_wavelet_icm import shrink_mrf2 as bws2d
+#
+#from mrf_wavelet_icm import shrink_mrf1_icm as bws1d
+#
+#from mrf_wavelet_icm import redescend_residuals_logistic
+#from mrf_wavelet_icm import get_redescend_weights_logistic
+#
+#
+#__all__ = ["bws1d","bws2d","bws2d_redescend"]
---
+++ redescendl/redescend_likelihood.pyx
@@ -0,0 +1,113 @@
+import pywt
+import numpy 
+cimport numpy
+
+from libc.math cimport exp, sqrt
+from libc.math cimport M_PI
+
+
+
+
+
+#cdef numpy.ndarray[numpy.float64_t,ndim=2] redescend_residuals(
+def redescend_residuals_logistic(
+		numpy.ndarray[numpy.float64_t,ndim=2] resids, double cval):
+
+	return get_redescend_weights_logistic(resids, cval)*resids
+
+
+def redescend_residuals_normal(
+		numpy.ndarray[numpy.float64_t,ndim=2] resids, double cval):
+
+	return get_redescend_weights_normal(resids, cval)*resids
+
+
+#cdef numpy.ndarray[numpy.float64_t,ndim=2] get_redescend_weights(
+def get_redescend_weights_logistic(
+		numpy.ndarray[numpy.float64_t,ndim=2] resids,double cval):
+
+	cdef double sval = resids.std()*sqrt(3.0)/M_PI
+
+	cdef numpy.ndarray[numpy.float64_t,ndim=2] prob_vec1 = \
+			prob_logistic_vec_2d(resids,sval)
+
+	cdef numpy.ndarray[numpy.float64_t,ndim=2] prob_vec2 = \
+			prob_logistic_vec_2d(resids,cval*sval)
+
+	cdef numpy.ndarray[numpy.float64_t,ndim=2] likelihood_weights = \
+		prob_vec1/(prob_vec1 + prob_vec2)
+
+	cdef double prob1 = prob_logistic_scalar(0.0, sval)
+	cdef double prob2 = prob_logistic_scalar(0.0, cval*sval)
+
+	likelihood_weights /= prob1/(prob1 + prob2)
+
+	return likelihood_weights
+
+
+#cdef numpy.ndarray[numpy.float64_t,ndim=2] get_redescend_weights(
+def get_redescend_weights_normal(
+		numpy.ndarray[numpy.float64_t,ndim=2] resids, double cval):
+
+	cdef double stdev = resids.std()
+
+	cdef numpy.ndarray[numpy.float64_t,ndim=2] prob_vec1 = \
+			prob_normal_vec_2d(resids,stdev)
+
+	cdef numpy.ndarray[numpy.float64_t,ndim=2] prob_vec2 = \
+			prob_normal_vec_2d(resids,cval*stdev)
+
+	cdef numpy.ndarray[numpy.float64_t,ndim=2] likelihood_weights = \
+		prob_vec1/(prob_vec1 + prob_vec2)
+
+	cdef double prob1 = prob_normal_scalar(0.0, stdev)
+	cdef double prob2 = prob_normal_scalar(0.0, cval*stdev)
+
+	likelihood_weights /= prob1/(prob1 + prob2)
+
+	return likelihood_weights
+
+
+cdef double prob_logistic_scalar(double val, double s):
+	cdef double numerator = exp(-val/s)
+	cdef double denominator = 1.0 + numerator
+	denominator *= s*denominator
+
+	return numerator/denominator
+
+
+cdef numpy.ndarray[numpy.float64_t,ndim=1] prob_logistic_vec_1d(
+		numpy.ndarray[numpy.float64_t,ndim=1] vec, double s):
+
+	cdef numpy.ndarray[numpy.float64_t,ndim=1] numerator = exp(-vec/s)
+	cdef numpy.ndarray[numpy.float64_t,ndim=1] denominator = \
+		1.0 + numerator
+	denominator *= s*denominator
+
+	return numerator/denominator
+
+
+cdef numpy.ndarray[numpy.float64_t,ndim=2] \
+	prob_logistic_vec_2d(numpy.ndarray[numpy.float64_t,ndim=2] vec, double s):
+	cdef numpy.ndarray[numpy.float64_t,ndim=2] numerator = numpy.exp(-vec/s)
+	cdef numpy.ndarray[numpy.float64_t,ndim=2] denominator = \
+		1.0 + numerator
+	denominator *= s*denominator
+
+	return numerator/denominator
+
+
+cdef double prob_normal_scalar(double val, double s):
+	cdef double prob = numpy.exp(-0.5*(val/s)**2)
+
+	return prob/(s*sqrt(2.0*M_PI))
+
+
+cdef numpy.ndarray[numpy.float64_t,ndim=2] \
+	prob_normal_vec_2d(numpy.ndarray[numpy.float64_t,ndim=2] vec, double s):
+
+	cdef numpy.ndarray[numpy.float64_t,ndim=2] prob_vec = \
+			numpy.exp(-0.5*(vec/s)**2)
+
+	return prob_vec/(s*sqrt(2.0*M_PI))
+
