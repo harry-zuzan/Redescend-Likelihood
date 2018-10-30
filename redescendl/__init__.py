43782524cf37d56e696d1490bd0a1c19267b4598
Harry Zuzan <iliketobicycle@gmail.com>
2018-10-30 10:02:27 -0700

    made changes to __init__.py to be compatible with Python 3.x

--- redescendl/__init__.py
+++ redescendl/__init__.py
@@ -1,31 +1,30 @@
-from redescend_likelihood \
+from .redescend_likelihood \
 	import redescend_residuals_normal_1d as redescend_normal1
 
-from redescend_likelihood \
+from .redescend_likelihood \
 	import redescend_residuals_normal_2d as redescend_normal2
 
-from redescend_likelihood \
+from .redescend_likelihood \
 	import redescend_residuals_normal_3d as redescend_normal3
 
-from redescend_likelihood \
+from .redescend_likelihood \
 	import get_redescend_weights_normal_1d as get_weights_normal1
 
-from redescend_likelihood \
+from .redescend_likelihood \
 	import get_redescend_weights_normal_2d as get_weights_normal2
 
-from redescend_likelihood \
+from .redescend_likelihood \
 	import get_redescend_weights_normal_3d as get_weights_normal3
 
-
-
-from redescend_likelihood \
+from .redescend_likelihood \
 	import redescend_residuals_logistic_2d as redescend_logistic2
 
-from redescend_likelihood \
+from .redescend_likelihood \
 	import get_redescend_weights_logistic_2d as get_weights_logistic2
+
+del redescend_likelihood
 
 __all__ = ["redescend_normal2", "redescend_normal1"]
 __all__ += ["get_weights_normal2", "get_weights_normal1"]
 __all__ += ["redescend_logistic2"]
 
-del redescend_likelihood
